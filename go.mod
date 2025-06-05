87e803df921d83e0ff8faeb744f96e80ea8f8cac
Eckorezze <alexandrsoloby@gmail.com>
2025-06-05 13:07:27 +0300

    upd logger

--- go.mod
+++ go.mod
@@ -1,35 +1,35 @@
 module github.com/Zorynix/shared
 
-go 1.23
-
-toolchain go1.24.2
+go 1.24.2
 
 require (
 	github.com/go-playground/validator/v10 v10.26.0
 	github.com/go-redis/redis/v8 v8.11.5
 	github.com/prometheus/client_golang v1.22.0
-	github.com/sirupsen/logrus v1.9.3
 	google.golang.org/grpc v1.72.2
 	gopkg.in/yaml.v3 v3.0.1
 )
+
+require go.uber.org/multierr v1.10.0 // indirect
 
 require (
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
-	github.com/gabriel-vasile/mimetype v1.4.8 // indirect
+	github.com/gabriel-vasile/mimetype v1.4.9 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/kr/text v0.2.0 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
-	github.com/prometheus/client_model v0.6.1 // indirect
-	github.com/prometheus/common v0.62.0 // indirect
-	github.com/prometheus/procfs v0.15.1 // indirect
-	golang.org/x/crypto v0.33.0 // indirect
-	golang.org/x/net v0.35.0 // indirect
-	golang.org/x/sys v0.30.0 // indirect
-	golang.org/x/text v0.22.0 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20250218202821-56aae31c358a // indirect
-	google.golang.org/protobuf v1.36.5 // indirect
+	github.com/prometheus/client_model v0.6.2 // indirect
+	github.com/prometheus/common v0.64.0 // indirect
+	github.com/prometheus/procfs v0.16.1 // indirect
+	go.uber.org/zap v1.27.0
+	golang.org/x/crypto v0.38.0 // indirect
+	golang.org/x/net v0.40.0 // indirect
+	golang.org/x/sys v0.33.0 // indirect
+	golang.org/x/text v0.25.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20250603155806-513f23925822 // indirect
+	google.golang.org/protobuf v1.36.6 // indirect
 )
--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -3,10 +3,20 @@ package logger
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"os"
 	"time"
 
-	"github.com/sirupsen/logrus"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+const (
+	EnvLocal       = "local"
+	EnvDev         = "dev"
+	EnvDevelopment = "development"
+	EnvProd        = "prod"
+	EnvProduction  = "production"
 )
 
 type ContextKey string
@@ -20,8 +30,9 @@ const (
 )
 
 type Logger struct {
-	*logrus.Logger
+	*zap.Logger
 	serviceName string
+	environment string
 }
 
 type LogLevel string
@@ -39,160 +50,307 @@ type Config struct {
 	Format      string   `yaml:"format" validate:"required,oneof=json text"`
 	ServiceName string   `yaml:"service_name" validate:"required"`
 	Environment string   `yaml:"environment" validate:"required"`
-	Output      string   `yaml:"output" validate:"oneof=stdout stderr file"`
-	FilePath    string   `yaml:"file_path,omitempty"`
+	Output      string   `yaml:"output" validate:"required,oneof=stdout stderr file"`
+	FilePath    string   `yaml:"file_path"`
 }
 
 type Fields map[string]interface{}
 
+func New(env string) *Logger {
+	var logger *zap.Logger
+
+	switch env {
+	case EnvLocal, EnvDev, EnvDevelopment:
+		logger = newDevelopmentLogger()
+	case EnvProd, EnvProduction:
+		logger = newProductionLogger()
+	default:
+		logger = newDevelopmentLogger()
+	}
+
+	return &Logger{
+		Logger:      logger,
+		environment: env,
+	}
+}
+
 func NewLogger(config Config) (*Logger, error) {
-	logger := logrus.New()
-
-	level, err := logrus.ParseLevel(string(config.Level))
-	if err != nil {
-		return nil, err
-	}
-	logger.SetLevel(level)
-
-	switch config.Format {
-	case "json":
-		logger.SetFormatter(&logrus.JSONFormatter{
-			TimestampFormat: time.RFC3339Nano,
-			FieldMap: logrus.FieldMap{
-				logrus.FieldKeyTime:  "timestamp",
-				logrus.FieldKeyLevel: "level",
-				logrus.FieldKeyMsg:   "message",
-			},
-		})
-	case "text":
-		logger.SetFormatter(&logrus.TextFormatter{
-			TimestampFormat: time.RFC3339Nano,
-			FullTimestamp:   true,
-		})
+	var level zapcore.Level
+	switch config.Level {
+	case LevelDebug:
+		level = zap.DebugLevel
+	case LevelInfo:
+		level = zap.InfoLevel
+	case LevelWarn:
+		level = zap.WarnLevel
+	case LevelError:
+		level = zap.ErrorLevel
+	case LevelFatal:
+		level = zap.FatalLevel
+	default:
+		level = zap.InfoLevel
 	}
 
+	var encoderConfig zapcore.EncoderConfig
+	var encoder zapcore.Encoder
+
+	if config.Format == "json" {
+		encoderConfig = zap.NewProductionEncoderConfig()
+		encoderConfig.TimeKey = "timestamp"
+		encoderConfig.LevelKey = "level"
+		encoderConfig.MessageKey = "message"
+		encoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+		encoderConfig.EncodeLevel = zapcore.LowercaseLevelEncoder
+		encoder = zapcore.NewJSONEncoder(encoderConfig)
+	} else {
+		encoderConfig = zap.NewDevelopmentEncoderConfig()
+		encoderConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
+		encoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+		encoder = zapcore.NewConsoleEncoder(encoderConfig)
+	}
+
+	var writeSyncer zapcore.WriteSyncer
 	switch config.Output {
 	case "stderr":
-		logger.SetOutput(os.Stderr)
+		writeSyncer = zapcore.AddSync(os.Stderr)
 	case "file":
 		if config.FilePath == "" {
-			return nil, logrus.NewEntry(logger).WithError(err).Logger
+			return nil, fmt.Errorf("file path is required when output is set to file")
 		}
 		file, err := os.OpenFile(config.FilePath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
 		if err != nil {
 			return nil, err
 		}
-		logger.SetOutput(file)
+		writeSyncer = zapcore.AddSync(file)
 	default:
-		logger.SetOutput(os.Stdout)
+		writeSyncer = zapcore.AddSync(os.Stdout)
 	}
 
+	core := zapcore.NewCore(encoder, writeSyncer, level)
+	zapLogger := zap.New(core, zap.AddCaller(), zap.AddStacktrace(zap.ErrorLevel))
+
 	return &Logger{
-		Logger:      logger,
+		Logger:      zapLogger,
 		serviceName: config.ServiceName,
+		environment: config.Environment,
 	}, nil
 }
 
-func (l *Logger) WithContext(ctx context.Context) *logrus.Entry {
-	entry := l.Logger.WithFields(logrus.Fields{
-		"service": l.serviceName,
-	})
+func newDevelopmentLogger() *zap.Logger {
+	encoderConfig := zap.NewDevelopmentEncoderConfig()
+	encoderConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
+	encoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+
+	consoleEncoder := zapcore.NewConsoleEncoder(encoderConfig)
+	writeSyncer := zapcore.AddSync(os.Stdout)
+	core := zapcore.NewCore(consoleEncoder, writeSyncer, zap.DebugLevel)
+
+	return zap.New(core,
+		zap.AddCaller(),
+		zap.AddStacktrace(zap.ErrorLevel),
+	)
+}
+
+func newProductionLogger() *zap.Logger {
+	encoderConfig := zap.NewProductionEncoderConfig()
+	encoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+
+	encoderConfig.EncodeCaller = func(caller zapcore.EntryCaller, enc zapcore.PrimitiveArrayEncoder) {
+		enc.AppendString(caller.TrimmedPath())
+	}
+
+	config := zap.Config{
+		Level:             zap.NewAtomicLevelAt(zap.InfoLevel),
+		Development:       false,
+		Encoding:          "json",
+		EncoderConfig:     encoderConfig,
+		OutputPaths:       []string{"stdout"},
+		ErrorOutputPaths:  []string{"stderr"},
+		DisableCaller:     false,
+		DisableStacktrace: false,
+	}
+
+	logger, _ := config.Build()
+	return logger
+}
+
+func (l *Logger) With(fields ...zapcore.Field) *Logger {
+	return &Logger{
+		Logger:      l.Logger.With(fields...),
+		serviceName: l.serviceName,
+		environment: l.environment,
+	}
+}
+
+func (l *Logger) WithComponent(component string) *Logger {
+	return &Logger{
+		Logger:      l.Logger.With(zap.String("component", component)),
+		serviceName: l.serviceName,
+		environment: l.environment,
+	}
+}
+
+func (l *Logger) WithService(service string) *Logger {
+	return &Logger{
+		Logger:      l.Logger.With(zap.String("service", service)),
+		serviceName: service,
+		environment: l.environment,
+	}
+}
+
+func (l *Logger) Sync() {
+	_ = l.Logger.Sync()
+}
+
+func (l *Logger) WithContext(ctx context.Context) *Logger {
+	fields := make([]zapcore.Field, 0, 5)
+
+	if l.serviceName != "" {
+		fields = append(fields, zap.String("service", l.serviceName))
+	}
 
 	if requestID := ctx.Value(RequestIDKey); requestID != nil {
-		entry = entry.WithField("request_id", requestID)
+		if rid, ok := requestID.(string); ok {
+			fields = append(fields, zap.String("request_id", rid))
+		}
 	}
 	if userID := ctx.Value(UserIDKey); userID != nil {
-		entry = entry.WithField("user_id", userID)
+		if uid, ok := userID.(string); ok {
+			fields = append(fields, zap.String("user_id", uid))
+		}
 	}
 	if operation := ctx.Value(OperationKey); operation != nil {
-		entry = entry.WithField("operation", operation)
+		if op, ok := operation.(string); ok {
+			fields = append(fields, zap.String("operation", op))
+		}
 	}
 	if traceID := ctx.Value(TraceIDKey); traceID != nil {
-		entry = entry.WithField("trace_id", traceID)
+		if tid, ok := traceID.(string); ok {
+			fields = append(fields, zap.String("trace_id", tid))
+		}
 	}
 
-	return entry
+	return &Logger{
+		Logger:      l.Logger.With(fields...),
+		serviceName: l.serviceName,
+		environment: l.environment,
+	}
 }
 
-func (l *Logger) WithFields(fields Fields) *logrus.Entry {
-	logrusFields := make(logrus.Fields)
+func (l *Logger) WithFields(fields Fields) *Logger {
+	zapFields := make([]zapcore.Field, 0, len(fields)+1)
+
+	if l.serviceName != "" {
+		zapFields = append(zapFields, zap.String("service", l.serviceName))
+	}
+
 	for k, v := range fields {
-		logrusFields[k] = v
+		zapFields = append(zapFields, zap.Any(k, v))
 	}
-	logrusFields["service"] = l.serviceName
-	return l.Logger.WithFields(logrusFields)
+
+	return &Logger{
+		Logger:      l.Logger.With(zapFields...),
+		serviceName: l.serviceName,
+		environment: l.environment,
+	}
 }
 
-func (l *Logger) WithError(err error) *logrus.Entry {
-	return l.Logger.WithError(err).WithField("service", l.serviceName)
+func (l *Logger) WithError(err error) *Logger {
+	fields := []zapcore.Field{zap.Error(err)}
+	if l.serviceName != "" {
+		fields = append(fields, zap.String("service", l.serviceName))
+	}
+
+	return &Logger{
+		Logger:      l.Logger.With(fields...),
+		serviceName: l.serviceName,
+		environment: l.environment,
+	}
 }
 
 func (l *Logger) LogRequest(ctx context.Context, method, path string, statusCode int, duration time.Duration) {
-	l.WithContext(ctx).WithFields(logrus.Fields{
-		"http_method":      method,
-		"http_path":        path,
-		"http_status":      statusCode,
-		"response_time_ms": duration.Milliseconds(),
-		"type":             "http_request",
-	}).Info("HTTP request processed")
+	l.WithContext(ctx).Info("HTTP request processed",
+		zap.String("http_method", method),
+		zap.String("http_path", path),
+		zap.Int("http_status", statusCode),
+		zap.Int64("response_time_ms", duration.Milliseconds()),
+		zap.String("type", "http_request"),
+	)
 }
 
 func (l *Logger) LogGRPCRequest(ctx context.Context, method string, duration time.Duration, err error) {
-	fields := logrus.Fields{
-		"grpc_method":      method,
-		"response_time_ms": duration.Milliseconds(),
-		"type":             "grpc_request",
+	logger := l.WithContext(ctx)
+	fields := []zapcore.Field{
+		zap.String("grpc_method", method),
+		zap.Int64("response_time_ms", duration.Milliseconds()),
+		zap.String("type", "grpc_request"),
 	}
 
-	entry := l.WithContext(ctx).WithFields(fields)
 	if err != nil {
-		entry.WithError(err).Error("gRPC request failed")
+		logger.Error("gRPC request failed", append(fields, zap.Error(err))...)
 	} else {
-		entry.Info("gRPC request processed")
+		logger.Info("gRPC request processed", fields...)
 	}
 }
 
 func (l *Logger) LogDatabaseQuery(ctx context.Context, query string, duration time.Duration, err error) {
-	fields := logrus.Fields{
-		"db_query":      query,
-		"query_time_ms": duration.Milliseconds(),
-		"type":          "database_query",
+	logger := l.WithContext(ctx)
+	fields := []zapcore.Field{
+		zap.String("db_query", query),
+		zap.Int64("query_time_ms", duration.Milliseconds()),
+		zap.String("type", "database_query"),
 	}
 
-	entry := l.WithContext(ctx).WithFields(fields)
 	if err != nil {
-		entry.WithError(err).Error("Database query failed")
+		logger.Error("Database query failed", append(fields, zap.Error(err))...)
 	} else {
-		entry.Debug("Database query executed")
+		logger.Debug("Database query executed", fields...)
 	}
 }
 
 func (l *Logger) LogCacheOperation(ctx context.Context, operation, key string, hit bool, duration time.Duration) {
-	l.WithContext(ctx).WithFields(logrus.Fields{
-		"cache_operation":   operation,
-		"cache_key":         key,
-		"cache_hit":         hit,
-		"operation_time_ms": duration.Milliseconds(),
-		"type":              "cache_operation",
-	}).Debug("Cache operation")
+	l.WithContext(ctx).Debug("Cache operation",
+		zap.String("cache_operation", operation),
+		zap.String("cache_key", key),
+		zap.Bool("cache_hit", hit),
+		zap.Int64("operation_time_ms", duration.Milliseconds()),
+		zap.String("type", "cache_operation"),
+	)
 }
 
 func (l *Logger) LogBusinessEvent(ctx context.Context, event string, data interface{}) {
 	dataJSON, _ := json.Marshal(data)
-	l.WithContext(ctx).WithFields(logrus.Fields{
-		"business_event": event,
-		"event_data":     string(dataJSON),
-		"type":           "business_event",
-	}).Info("Business event occurred")
+	l.WithContext(ctx).Info("Business event occurred",
+		zap.String("business_event", event),
+		zap.String("event_data", string(dataJSON)),
+		zap.String("type", "business_event"),
+	)
 }
 
 func (l *Logger) LogSecurity(ctx context.Context, event, details string, severity string) {
-	l.WithContext(ctx).WithFields(logrus.Fields{
-		"security_event":   event,
-		"security_details": details,
-		"severity":         severity,
-		"type":             "security_event",
-	}).Warn("Security event")
+	l.WithContext(ctx).Warn("Security event",
+		zap.String("security_event", event),
+		zap.String("security_details", details),
+		zap.String("severity", severity),
+		zap.String("type", "security_event"),
+	)
+}
+
+func Err(err error) zapcore.Field {
+	return zap.Error(err)
+}
+
+func Discard() *Logger {
+	return &Logger{zap.NewNop(), "", ""}
+}
+
+func Test() *Logger {
+	core := zapcore.NewCore(
+		zapcore.NewConsoleEncoder(zap.NewDevelopmentEncoderConfig()),
+		zapcore.AddSync(os.Stderr),
+		zap.NewAtomicLevelAt(zap.DebugLevel),
+	)
+	return &Logger{zap.New(core), "test", "test"}
 }
 
 func CreateContextWithRequestID(ctx context.Context, requestID string) context.Context {
